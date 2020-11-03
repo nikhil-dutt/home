f04a86c40396fcd09003499e88a0871eb1604a37
Nikhil Dutt <nikhil.dutt11@yahoo.com>
2020-11-02 21:02:43 -0800

    Updates

---
+++ precache-manifest.b3495b3899428129dfe78f1f453d25e7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f4f23715513cdcdb87eb7cfbe5e6bc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23cdda33fe705794b769",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "2c0850d5c461ee2f66be",
+    "url": "/static/js/2.97cffbda.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.97cffbda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23cdda33fe705794b769",
+    "url": "/static/js/main.de183ba7.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "7cf878898ae9d22f2416824a83aaf9f0",
+    "url": "/static/media/IMG_4124.7cf87889.jpg"
+  },
+  {
+    "revision": "7eada0f40cc15a3312485347ed132d9e",
+    "url": "/static/media/Nikhil_Dutt_Resume_2020.7eada0f4.pdf"
+  },
+  {
+    "revision": "84cc7d6ba3fcba6ab2db28032536a0e0",
+    "url": "/static/media/ai.84cc7d6b.jpg"
+  },
+  {
+    "revision": "3a42f85028dee8263336f53caaf205fa",
+    "url": "/static/media/cow.3a42f850.JPG"
+  },
+  {
+    "revision": "b75f1bcb731387a7a8285af5c3307d48",
+    "url": "/static/media/ed cross.b75f1bcb.JPG"
+  },
+  {
+    "revision": "41ea361a017f6a87e6e7de2dbc574265",
+    "url": "/static/media/ewsarticle.41ea361a.png"
+  },
+  {
+    "revision": "19a79a91bdc932f4c7220b67974d467f",
+    "url": "/static/media/flba.19a79a91.jpg"
+  },
+  {
+    "revision": "8f0596e4a35a34caa1a05cdd72123fdd",
+    "url": "/static/media/houdini.8f0596e4.png"
+  },
+  {
+    "revision": "043d640dc731d9f977fd5ed4418f2dc6",
+    "url": "/static/media/picture of nikhil.043d640d.jpeg"
+  }
+]);
